cc1c7e180430ec80129d67e9caf34180d4775da2
RitvarsZ <ritvarszvejnieks@protonmail.com>
2023-02-05 22:43:10 +0200

    Improve transitions.

--- resources/js/Components/ApartmentPreview.vue
+++ resources/js/Components/ApartmentPreview.vue
@@ -1,16 +1,36 @@
 <script setup>
 import SecondaryButton from './SecondaryButton.vue';
 import PrimaryButton from './PrimaryButton.vue';
+import { ref, watch } from 'vue';
 
 const props = defineProps({
     apartment: { type: Object, default: null },
 });
+
+const emit = defineEmits(['close']);
+
+const isOpen = ref(false);
+
+const onClose = () => {
+    isOpen.value = false;
+    setTimeout(() => {
+        emit('close');
+    }, 300);
+}
+
+// watch apartment prop
+watch(() => props.apartment, (apartment) => {
+    if (apartment) {
+        isOpen.value = true;
+    }
+});
+
 </script>
 
 <template>
-    <div class="fixed right-0 top-0 h-full w-10/12 sm:w-1/3 bg-slate-100 shadow-lg p-4 transition-all duration-300 ease-in-out overflow-hidden"
-        :class="{ 'translate-x-full' : apartment === null }">
-        <SecondaryButton class="float-right" @click="$emit('close')">Aizvērt</SecondaryButton>
+    <div class="fixed right-0 bottom-0 md:bottom-1/3 w-full md:w-1/2 lg:w-1/3 bg-slate-100 drop-shadow-xl p-6 pb-10 rounded-2xl transition-all duration-300 ease-in-out overflow-hidden z-50"
+        :class="{ 'translate-y-full md:translate-y-0 md:translate-x-full' : !isOpen }">
+        <SecondaryButton class="float-right" @click="onClose">Aizvērt</SecondaryButton>
         <template v-if="apartment">
             <p>Pievienots: {{ apartment.published_at }}</p>
             <img :src="apartment.image_thumbnail" alt="thumb">
